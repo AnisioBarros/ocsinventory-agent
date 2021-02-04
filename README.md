f997baf8d6c844f905cfc49b741df44b6366caad
Anisio <anisiobarros96@gmail.com>
2021-02-04 15:49:03 -0300

    M - melhoria no script check dir

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Faça o clone do repositório com o comando:
+## Execute o comando: 
+
+- curl -s https://raw.githubusercontent.com/AnisioBarros/ocsinventory-agent/master/ocs.sh | sudo  bash -s
+
+# Caso o comando acima não funcione, faça o clone do repositório com o comando:
 
 - git clone https://github.com/AnisioBarros/ocsinventory-agent.git
 
--- ocs.sh
+++ ocs.sh
@@ -1,22 +1,73 @@
 #!/bin/bash -x
+
+DIR="/etc/ocsinventory/"
+if [ -d "$DIR" ]; then
+  echo "diretorio ${DIR} ja existe"
+else
+  echo "criando diretorio ${DIR}"
+  mkdir -p /etc/ocsinventory/
+  # exit 1
+fi
+
 echo -n "Criando diretorios e dando permissoes... "
-mkdir -p /etc/ocsinventory/
-touch /etc/ocsinventory/ocsinventory-agent.cfg
-chmod 0766 /etc/ocsinventory/ocsinventory-agent.cfg
-sleep 0.5; echo -n " [ OK ]"; echo 
-echo -n "Carregando informacoes do servidor ocs... "
-echo "server=http://ocs.dev.infra.mateus/ocsinventory" > /etc/ocsinventory/ocsinventory-agent.cfg
+
+FILE="/etc/ocsinventory/ocsinventory-agent.cfg"
+if [ -f "$FILE" ]; then
+    echo "arquivo ja existe, vou atualizar"
+    touch ${FILE} ;
+    chmod 0766 ${FILE} ;
+        if [ $? -eq 0 ]
+        then
+        echo "arquivo criado com sucesso"
+        else
+        echo "nao foi possivel criar o importa.sh"
+        chattr -i ${FILE}
+        touch ${FILE} ;
+        chmod 0766 ${FILE} ;
+        fi
+    sleep 0.5; echo -n " [ OK ]"; echo 
+    echo -n "Carregando informacoes do servidor ocs... "
+    echo "server=http://ocs.dev.infra.mateus/ocsinventory" > ${FILE} ;
+    sleep 2 ; 
+else 
+    echo "${FILE} nao existe, vou criar."
+    touch ${FILE} ;
+    chmod 0766 ${FILE} ;
+        if [ $? -eq 0 ]
+        then
+        echo "arquivo criado com sucesso"
+        else
+        echo "nao foi possivel criar o arquivo"
+        chattr -i ${FILE}
+        fi    
+    sleep 0.5; echo -n " [ OK ]"; echo 
+    echo -n "Carregando informacoes do servidor ocs... "        
+    echo "server=http://ocs.dev.infra.mateus/ocsinventory" > ${FILE} ;
+    sleep 2 ;     
+fi
 sleep 0.5; echo -n " [ OK ]"; echo 
 echo -n "Instalando agente do ocs... "
 apt update -y ; 
 DEBIAN_FRONTEND=noninteractive apt install -y ocsinventory-agent
-cp /etc/cron.hourly/ocsinventory-agent /etc/cron.daily/ocsinventory-agent
-cp /etc/cron.daily/ocsinventory-agent /etc/cron.hourly/ocsinventory-agent
+FILE="/etc/cron.daily/ocsinventory-agent"
 echo -n "Agendando envio de dados de hora em hora... "
 sleep 1.5; echo -n " [ OK ]"; echo 
-chown bin:bin /etc/cron.hourly/ocsinventory-agent
-chmod 0766 /etc/cron.hourly/ocsinventory-agent
-/etc/init.d/cron restart
-echo -n "Restart do cron... "
-bash /etc/cron.hourly/ocsinventory-agent
-sleep 1.5; echo -n " [ OK ]"; echo 
+if [ -f "$FILE" ]; then
+    echo "arquivo ja existe, vou atualizar"
+    cp ${FILE} /etc/cron.hourly/ocsinventory-agent
+    # chown bin:bin /etc/cron.hourly/ocsinventory-agent
+    # chmod 0766 /etc/cron.hourly/ocsinventory-agent
+    sleep 2 ; 
+    /etc/init.d/cron restart
+    echo -n "Restart do cron... "
+    bash /etc/cron.hourly/ocsinventory-agent
+    sleep 1.5; echo -n " [ OK ]"; echo 
+else 
+    echo "arquivo nao existe, vou criar"
+    cp /etc/cron.hourly/ocsinventory-agent ${FILE}
+    sleep 2 ;  
+    /etc/init.d/cron restart
+    echo -n "Restart do cron... "
+    bash /etc/cron.hourly/ocsinventory-agent
+    sleep 1.5; echo -n " [ OK ]"; echo 
+fi
